4185a27b5da0cd2cbdd3f62f9e38512c9f79046b
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-10-14 20:38:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ if array.length == 1
   then array.join
   elsif array.length == 2 
   then array.join(" and ")
-  elsif array.length == 3
+  elsif array.length >= 3
   then new = "and #{array[-1]}"
   array.pop
   array << new
